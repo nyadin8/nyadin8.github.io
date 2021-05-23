aa6d510510035e179191dce879e25c514105be5e
Alexander Shuhin <a.medium@gmail.com>
2021-05-24 00:47:03 +0300

    Add calculator

---
+++ calc/script.js
@@ -0,0 +1,32 @@
+const TARGETS = [[0, 3], [3, 2], [5, 1]];
+
+const calculateCoeff = (errorPerc) => {
+  if (errorPerc > R.last(TARGETS)[0]) {
+    return 1;
+  };
+
+  const targetIndex = R.findLastIndex(R.compose(R.gte(errorPerc), R.nth(0)), TARGETS);
+  const [x0, y0] = TARGETS[targetIndex];
+  const [x1, y1] = TARGETS[targetIndex+1];
+
+  const x = errorPerc;
+  const y = (y0 * (x1 - x) + y1 * (x - x0)) / (x1 - x0);
+
+  return y;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const numberInput = document.querySelector('#number');
+  const errorPercInput = document.querySelector('#errorPerc');
+  const calculateButton = document.querySelector('#calculate');
+  const resultContainer = document.querySelector('#result');
+
+  calculateButton.addEventListener('click', () => {
+    const numberValue = parseInt(numberInput.value);
+    const errorPercValue = parseFloat(errorPercInput.value);
+    const coeff = calculateCoeff(errorPercValue)
+    const result = Number(numberValue * coeff);
+
+    resultContainer.innerHTML = `Результат: ${result} руб.`;
+  });
+});
